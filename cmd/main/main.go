dc9fe9bc9e552921779d260afc848e7735c3ea1c
Sorokin Gleb <sorokingle06@yandex.ru>
2025-10-21 22:44:51 +0300

    fix: added fix of cfg

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net"
 	pb "orderService/api"
-	"orderService/internal/config"
 	v1 "orderService/internal/v1"
 	"orderService/pkg/repository"
 	"orderService/pkg/repository/mapstorage"
@@ -14,17 +13,14 @@ import (
 )
 
 func main() {
-	cfg, err := config.ParseConfig("internal/config/.env")
-	if err != nil {
-		log.Fatalf("main/ParseConfig: Ошибка загрузки config: %v", err)
-	}
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", cfg.Port))
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 50051))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
 	repo := repository.NewOrderService(mapstorage.NewMapStorage())
 	pb.RegisterOrderServiceServer(grpcServer, v1.NewServer(*repo))
+	//log.Printf("gRPC server is running on localhost:%d", lis.Addr())
 	err = grpcServer.Serve(lis)
 	if err != nil {
 		log.Fatalf("main/grpcServer.Serve не удалось запустить сервер: %v", err)
